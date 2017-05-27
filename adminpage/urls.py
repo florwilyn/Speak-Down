4a2a2b27e60126fe31d745be8400718b22afdda5
Tilmo8 <santino_gaara@yahoo.com>
2017-05-28 02:33:25 +0800

    Added Sign-in

--- adminpage/urls.py
+++ adminpage/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
 	url(r'^admin/home/$', views.homepage, name='admin_page'),
 	url(r'^approve/$', views.approve, name='approve'),
 	url(r'^delete/$', views.delete, name='delete'),
+	url(r'^sign-in/$', views.sign_in, name='sign_in'),
+	url(r'^sign-out/$', views.sign_out, name='sign_out')
+
 ]
--- adminpage/views.py
+++ adminpage/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from forum.models import Forum
+
+
+def sign_in(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    context = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('/')
+        else:
+            context['error'] = 'Invalid Username or Password'
+            context['username'] = username
+    return render(request, 'sign_in.html', context=context)
+
+def sign_out(request):
+    logout(request)
+    return redirect('sign_in')
 
 def homepage(request):
 	forum = Forum.objects.filter(approved=False).order_by('-date')
